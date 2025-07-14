7c838fbbaeebf834a9698b0aa96c88de62a77507
Lehuni-Dev <lehunilagrace@gmail.com>
2025-07-14 23:42:21 +0100

    Mise à jour des fichiers SCSS et de la configuration Vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-        host: '127.0.0.1', // ← ton IP ici aussi
+        host: '192.168.150.199', // ← ton IP ici aussi
         },
     },
     plugins: [
